060be44767bef4a940f8391c1159b7f8017ecb38
Allen Boynton <aboynton@fullsail.edu>
2017-05-22 16:49:15 -0400

    Created files and frameworks
    Added needed frameworks.
    Created organized file system.

---
+++ PokeMatch/PokeMatch/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  PokeMatch
+//
+//  Created by Allen Boynton on 5/22/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+}
+
