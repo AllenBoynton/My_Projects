949398ef00ef08eae1ac3700cd5e018af134c404
Allen Boynton <aboynton@fullsail.edu>
2017-05-23 02:37:45 -0400

    Rewrote time functions
    Created time with Timer class.
    Created updateDisplay().

--- BrainGames/CE04/ViewController.swift
+++ BrainGames/CE04/ViewController.swift
@@ -45,8 +45,15 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     var cardCounter = 0
     
     // NSTimers for game counter and delays in revealed images
-    var timer = NSTimer(), timer1 = NSTimer(), timer2 = NSTimer(), timer3 = NSTimer()
+    var timer: Timer?
+    var timer1 = Timer(), timer2 = Timer(), timer3 = Timer()
     
+    // Time values instantiated
+    var startTime: Double = 0
+    var time: Double = 0
+    var elapsed: Double = 0
+    var display: String = ""
+
     // Audio player
     var cheering = AVAudioPlayer()
     var tadaSound = AVAudioPlayer()
@@ -54,30 +61,25 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     var chime = AVAudioPlayer()
     var wrongAnswer = AVAudioPlayer()
     
-    // Time values instantiated
-    var minutes: Int = 0
-    var seconds: Double = 0
-    var time: String = ""
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Initialize tap gesture
         for view in cardViewArray {
-            view.userInteractionEnabled = true
+            view.isUserInteractionEnabled = true
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapGesture))
             view.addGestureRecognizer(tapGesture)
         }
         
         // Hides card views before game play starts.
         for view in cardViewArray {
-            view.hidden = true
+            view.isHidden = true
             print("View hidden before starting - viewDidLoad")
         }
         
         // Check to see how many buttons are on the screen to determine iPhone or iPad.
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+        if UIDevice.current.userInterfaceIdiom == .phone {
             
             // iPhone array for 20 buttons
             cardImages = [
@@ -92,7 +94,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             // Call function to populate images
             randomImageLoop()
             
-        } else if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+        } else if UIDevice.current.userInterfaceIdiom == .pad {
             
             // iPad array for 30 buttons
             cardImages = [
@@ -117,35 +119,35 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         if let play = playButton {
             play.layer.cornerRadius = 6
             play.layer.borderWidth = 2
-            play.layer.borderColor = UIColor.blackColor().CGColor
+            play.layer.borderColor = UIColor.black.cgColor
         }
         
         // Start button attributes
         if let start = startButton {
             start.layer.cornerRadius = 6
             start.layer.borderWidth = 2
-            start.layer.borderColor = UIColor.blackColor().CGColor
+            start.layer.borderColor = UIColor.black.cgColor
         }
         
         if let start = startButton2 {
             start.layer.cornerRadius = 6
             start.layer.borderWidth = 2
-            start.layer.borderColor = UIColor.blackColor().CGColor
+            start.layer.borderColor = UIColor.black.cgColor
         }
         
         // Timer display attributes
         if let display = timerDisplay {
             display.layer.borderWidth = 2
-            display.layer.borderColor = UIColor.blackColor().CGColor
+            display.layer.borderColor = UIColor.black.cgColor
         }
         
         if let display = timerDisplay2 {
             display.layer.borderWidth = 2
-            display.layer.borderColor = UIColor.blackColor().CGColor
+            display.layer.borderColor = UIColor.black.cgColor
         }
         
         utilityView?.layer.borderWidth = 2
-        utilityView?.layer.borderColor = UIColor.yellowColor().CGColor
+        utilityView?.layer.borderColor = UIColor.yellow.cgColor
     }
     
     // for in loop to populate array images by removing a random one at a time while array depletes to ensure pairs
@@ -154,10 +156,10 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             if cardImages.count > 0 {
                 cardCounter += 1
                 let random = arc4random_uniform(UInt32(cardImages.count))
-                index.image = UIImage(named: cardImages.removeAtIndex(Int(random)))
+                index.image = UIImage(named: cardImages.remove(at: Int(random)))
                 index.layer.borderWidth = 2
-                index.layer.borderColor = UIColor.blackColor().CGColor
-                index.hidden = true
+                index.layer.borderColor = UIColor.black.cgColor
+                index.isHidden = true
                 print("Images populating & Images Hidden")
             }
         }
@@ -166,75 +168,94 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     // Sound files
     func prepareAudios() {
         
-        let audioPath1 = NSBundle.mainBundle().pathForResource("Cheering", ofType: "mp3")
-        let error1: NSError? = nil
+        let audioPath1 = Bundle.main.path(forResource: "Cheering", ofType: "mp3")!
+        
         do {
-            cheering = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath1!))
-        }
-        catch {
-            print("Something bad happened \(error1). Try catching specific errors to narrow things down")
+            cheering = try AVAudioPlayer(contentsOf: URL(string: audioPath1)!)
+            cheering.prepareToPlay()
+            
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
         }
         
-        let audioPath2 = NSBundle.mainBundle().pathForResource("taDa", ofType: "mp3")
-        let error2: NSError? = nil
+        let audioPath2 = Bundle.main.path(forResource: "taDa", ofType: "mp3")!
+        
         do {
-            tadaSound = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath2!))
-        }
-        catch {
-            print("Something bad happened \(error2). Try catching specific errors to narrow things down")
+            tadaSound = try AVAudioPlayer(contentsOf: URL(string: audioPath2)!)
+            tadaSound.prepareToPlay()
+            
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
         }
         
-        let audioPath3 = NSBundle.mainBundle().pathForResource("chime", ofType: "mp3")
-        let error3: NSError? = nil
+        let audioPath3 = Bundle.main.path(forResource: "chime", ofType: "mp3")!
+        
         do {
-            chime = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath3!))
-        }
-        catch {
-            print("Something bad happened \(error3). Try catching specific errors to narrow things down")
+            chime = try AVAudioPlayer(contentsOf: URL(string: audioPath3)!)
+            chime.prepareToPlay()
+            
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
         }
         
-        let audioPath5 = NSBundle.mainBundle().pathForResource("Pat", ofType: "mp3")
-        let error5: NSError? = nil
+        let audioPath4 = Bundle.main.path(forResource: "Pat", ofType: "mp3")!
+        
         do {
-            patSound = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath5!))
-        }
-        catch {
-            print("Something bad happened \(error5). Try catching specific errors to narrow things down")
+            patSound = try AVAudioPlayer(contentsOf: URL(string: audioPath4)!)
+            patSound.prepareToPlay()
+            
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
         }
     }
     
     // MARK: - Time counter
     
-    // Update the timer to the label
-    func gameCounter() -> String {
-        seconds += 1
-        time = seconds.timeString
-        
-        // Need to use optional binding on timerDisplay because it is showing nil 
-        if let label = timerDisplay {
-            label.text = time
-        } else {
-            timerDisplay?.text = time
-        }
-        
-        if let label = timerDisplay2 {
-            label.text = time
-        } else {
-            timerDisplay2?.text = time
-        }
-        print(time)
-        
-        return time
-    }
     
     // Starts time when play button is pressed.
     func startGameTime() {
-        self.timer.invalidate()
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.gameCounter), userInfo: nil, repeats: true)
+        
+        startTime = Date().timeIntervalSinceReferenceDate - elapsed
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+
+    // Updates game time on displays
+    func updateCounter () -> String {
+    
+        // Calculate total time since timer started in seconds
+        time = Date().timeIntervalSinceReferenceDate - startTime
+        
+        // Calculate minutes
+        let minutes = UInt8(time / 60.0)
+        time -= (TimeInterval(minutes) * 60)
+        
+        // Calculate seconds
+        let seconds = UInt8(time)
+        time -= TimeInterval(seconds)
+        
+        // Calculate milliseconds
+        let milliseconds = UInt8(time * 100)
+        
+        // Format time vars with leading zero
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strMilliseconds = String(format: "%02d", milliseconds)
+        
+        // Add time vars to relevant labels
+        display = "\(strMinutes):\(strSeconds):\(strMilliseconds)"
+        timerDisplay.text = display
+        timerDisplay2.text = display
+        print(display)
+        
+        return display
     }
     
     // Function assigns views with tap gestures
-    func tapGesture (sender: UITapGestureRecognizer) {
+    func tapGesture (_ sender: UITapGestureRecognizer) {
         
         // Checks if the user has selected more than 0 cards.
         if selectedCards.count > 0 {
@@ -242,7 +263,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             // if/else statement to deselect a card if the user retaps the same one.
             if sender.view?.description != selectedCards[0].description {
                 selectedCards.append(sender.view!)
-                (sender.view?.subviews[0] as! UIImageView).hidden = false
+                (sender.view?.subviews[0] as! UIImageView).isHidden = false
             } else {
                 // If 2 cards are selected and have different descriptions, the images are hidden.
                 keepCard()
@@ -252,7 +273,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         // If there isn't more than one box in the the array, we have to remove the description check or else that array index will be nil and will cause a crash. We still add the selected box to the array.
         } else {
             selectedCards.append(sender.view!)
-            (sender.view?.subviews[0] as! UIImageView).hidden = false
+            (sender.view?.subviews[0] as! UIImageView).isHidden = false
             print("Tapped - Image Added to Array")
         }
         
@@ -261,18 +282,18 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             
             // If statement checks subviews to see if they match.
             if (selectedCards[0].subviews[0] as! UIImageView).image == (selectedCards[1].subviews[0] as! UIImageView).image {
-                (sender.view?.subviews[0] as! UIImageView).hidden = false
+                (sender.view?.subviews[0] as! UIImageView).isHidden = false
                 // Disable user interaction so that they cannot tap any other cards.
-                view.userInteractionEnabled = false
+                view.isUserInteractionEnabled = false
                 
                 // Start a timer for .5 second so user can see the match. Calls removeCards function
-                timer2 = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(ViewController.removeCards), userInfo: nil, repeats: false)
+                timer2 = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.removeCards), userInfo: nil, repeats: false)
                 print("Tapped if 2 cards")
             } else {
-                (sender.view?.subviews[0] as! UIImageView).hidden = false
-                view.userInteractionEnabled = false
+                (sender.view?.subviews[0] as! UIImageView).isHidden = false
+                view.isUserInteractionEnabled = false
                 // Start timer for .5 second so user can see images were NOT a match. Calls hideCards function
-                timer3 = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(ViewController.keepCard), userInfo: nil, repeats: false)
+                timer3 = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.keepCard), userInfo: nil, repeats: false)
                 print("Tapped if 2 cards -else")
             }
         }
@@ -287,16 +308,16 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
         for card in selectedCards {
             card.layer.borderWidth = 2
-            card.layer.borderColor = UIColor.yellowColor().CGColor
-            card.backgroundColor = UIColor.blackColor()
-            (card.subviews[0] as! UIImageView).hidden = true
+            card.layer.borderColor = UIColor.yellow.cgColor
+            card.backgroundColor = UIColor.black
+            (card.subviews[0] as! UIImageView).isHidden = true
         }
         
         // Removes the selected boxes from the selected boxes array, allowing the user to start selecting more boxes.
-        selectedCards.removeAll(keepCapacity: false)
+        selectedCards.removeAll(keepingCapacity: false)
         
         // Renables user interaction.
-        view.userInteractionEnabled = true
+        view.isUserInteractionEnabled = true
     }
     
     // Function used to hide boxes when the user gets a match.
@@ -307,14 +328,14 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         chime.play()
 
         for card in selectedCards {
-            card.hidden = true
+            card.isHidden = true
         }
 
         // Removes the count of 1 card per selection so we know how many cards are left.
         cardCounter -= 1
         cardCounter -= 1
         
-        selectedCards.removeAll(keepCapacity: false)
+        selectedCards.removeAll(keepingCapacity: false)
         
         // Checks if the number of cards has reached 0.
         if cardCounter == 0 {
@@ -325,39 +346,39 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             cheering.play()
             
             // Stops the game counter once game is completed.
-            timer.invalidate()
+            timer?.invalidate()
             
             // Reveals the winner's screen.
-            gameView.hidden = true
-            winnersView.hidden = false
+            gameView.isHidden = true
+            winnersView.isHidden = false
             
             // Adds the winning text phrase to the winner's screen label.
-            winningTimeText.text! = "You finished in \(time) seconds!"
+            winningTimeText.text! = "You finished in \(display) seconds!"
         }
         
         // Enables the user's interaction once two boxes are matched.
-        view.userInteractionEnabled = true
+        view.isUserInteractionEnabled = true
     }
     
     // Function hides images for after the first 5 seconds game preview.
-    func seeInitialView(sender: UIButton) {
+    func seeInitialView(_ sender: UIButton) {
         
         // Enable user interaction to select views
-        view.userInteractionEnabled = true
+        view.isUserInteractionEnabled = true
         
         // Hides card Image and displays View to now select
         for images in cardImageArray {
-            images.hidden = true
+            images.isHidden = true
             print(" 3. Image Hidden - seeInitialView - After Preview")
         }
         
         for view in cardViewArray {
-            view.hidden = false
+            view.isHidden = false
             print("4. View NOT Hidden - seeInitialView - After Preview")
         }
     }
     
-    @IBAction func startButton(sender: UIButton) {
+    @IBAction func startButton(_ sender: UIButton) {
         
         // Button sound
         prepareAudios()
@@ -367,38 +388,38 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         startGameTime()
         
         // Ensures user can not tap buttons while showing 5 second preview.
-        view.userInteractionEnabled = false
+        view.isUserInteractionEnabled = false
         
         // Delay images to be shown for 5 seconds.
-        self.timer1 = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: #selector(ViewController.seeInitialView(_:)), userInfo: nil, repeats: false)
+        self.timer1 = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(ViewController.seeInitialView(_:)), userInfo: nil, repeats: false)
         
-        startButton.hidden = true
-        startButton2.hidden = true
+        startButton.isHidden = true
+        startButton2.isHidden = true
         
         // Displays Views and Images for the 5 second preview.
         for views in cardViewArray {
             views.layer.borderWidth = 2
-            views.layer.borderColor = UIColor.blackColor().CGColor
-            views.hidden = false
+            views.layer.borderColor = UIColor.black.cgColor
+            views.isHidden = false
             print(" 1. View NOT Hidden - startButton - Preview")
         }
         
         for images in cardImageArray {
-            images.hidden = false
+            images.isHidden = false
             print(" 2. Image NOT Hidden - startButton - Preview")
         }
     }
     
     // Button dismisses VC to return to Intro screen
-    @IBAction func restartGame(sender: UIButton) {
-        dismissViewControllerAnimated(true, completion: nil)
+    @IBAction func restartGame(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
         
         // Button sound 
         prepareAudios()
         patSound.play()
         
-        startButton.hidden = false
-        startButton2.hidden = false
+        startButton.isHidden = false
+        startButton2.isHidden = false
     }
 }
 
