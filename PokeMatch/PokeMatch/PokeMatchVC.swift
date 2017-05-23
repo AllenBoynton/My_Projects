738fbce115b2270f01b5c040f5757169389cf84e
Allen Boynton <aboynton@fullsail.edu>
2017-05-23 02:35:50 -0400

    Setup UI
    Created custom outlined text.
    Created custom drop shadow UIView.

---
+++ PokeMatch/PokeMatch/PokeMatchVC.swift
@@ -0,0 +1,42 @@
+//
+//  PokeMatchVC.swift
+//  PokeMatch
+//
+//  Created by Allen Boynton on 5/22/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PokeMatchVC: UIViewController {
+
+    @IBOutlet weak var pokeTitle: UILabel!
+    
+    
+    // Local variables
+    var pokeTiles = [Pokemon]()
+    var audioPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+    @IBAction func musicButtonPressed(_ sender: UIButton) {
+        
+        if audioPlayer.isPlaying {
+            // pauses music & makes partial transparent
+            audioPlayer.pause()
+            sender.alpha = 0.2
+            
+        } else {
+            // plays music & makes full view
+            audioPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
+}
+
