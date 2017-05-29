9df328b45a387332f1a06d0103825191d0331687
Allen Boynton <aboynton@fullsail.edu>
2017-05-29 03:45:23 -0400

    Delete files from nowhere

---
+++ BRYXBannerExample-cdfbqejqtuieulbrsgqhsisylcag/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-BRYXBannerExample.build/DerivedSources/Pods_BRYXBannerExample_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_BRYXBannerExampleVersionString[];
+ extern const double Pods_BRYXBannerExampleVersionNumber;
+
+ const unsigned char Pods_BRYXBannerExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_BRYXBannerExample  PROJECT:Pods-1" "\n";
+ const double Pods_BRYXBannerExampleVersionNumber __attribute__ ((used)) = (double)1.;
