4775f6f368358f0027a24318b306c91cb34dd3d2
Allen Boynton <alboynton4@gmail.com>
2017-07-12 18:42:08 -0400

    Checking bgMusic for nil
    It was crashing when sound is turned off in options and the main game
    view the sound button would show “ON.” If tapped, the app would crash.
    Fixed

--- PokeMatch/PokeMatch/OptionsVC.swift
+++ PokeMatch/PokeMatch/OptionsVC.swift
@@ -10,13 +10,12 @@ import UIKit
 import AVFoundation
 import GameKit
 
-
 class OptionsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let mainMenuVC = MainMenuVC()
     let pokeMatchVC = PokeMatchVC()
     
-    var musicPlayer = AVAudioPlayer()
+    let musicPlayer = AVAudioPlayer()
     
     // Outlets to initialize the items
     @IBOutlet weak var bgImage: UIImageView!
@@ -36,13 +35,12 @@ class OptionsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     // Create function to initiate music playing when game begins
     func startGameMusic() {
-        
         let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: "mp3")!)
         
         do {
             bgMusic = try AVAudioPlayer(contentsOf: url)
-            bgMusic.prepareToPlay()
-            bgMusic.play()
+            bgMusic?.prepareToPlay()
+            bgMusic?.play()
         } catch let error as NSError {
             print("audioPlayer error \(error.localizedDescription)")
         }
@@ -84,7 +82,6 @@ class OptionsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
             
     // Button changes game screens background
     @IBAction func changeBGImageBtnPressed(_ sender: UIButton) {
-        
         let alert = UIAlertController(title: "Would you like to personalize your background?", message: "Tap the image above the \"Change\" button to pick a New Image", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Change it!", style: .default , handler: nil))
@@ -109,15 +106,15 @@ class OptionsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     // Audio button mutes/unmutes music
     @IBAction func musicOptionSwitch(_ sender: UISwitch) {
-        if (sender.isOn == true) {
+        if sender.isOn {
             musicSwitch?.setOn(false, animated: true)
-            bgMusic.play()
+            startGameMusic()
         } else {
             musicSwitch?.setOn(true, animated: true)
             musicSwitch?.isOn = true
             
             if bgMusic != nil {
-                bgMusic.pause()
+                bgMusic?.pause()
                 bgMusic = nil
             }
         }
