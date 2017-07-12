aaa4c4c65f1c887f9e443aa2fdea572ce13a5690
Allen Boynton <alboynton4@gmail.com>
2017-07-12 18:43:08 -0400

    Created a restart button and view for end of game

--- PokeMatch/PokeMatch/WinnersVC.swift
+++ PokeMatch/PokeMatch/WinnersVC.swift
@@ -11,13 +11,20 @@ import GameKit
 
 class WinnersVC: UIViewController {
     
+    let pokeMatchVC = PokeMatchVC()
+    
     @IBOutlet weak var finalGamePoints: UILabel!
     @IBOutlet weak var finalGameTime: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var backgroundButton: UIButton!
     
     
     // Scores passed from PokeMatchVC
     var pointsPassed = ""
     var timePassed = ""
+    
+    // Bool whether restart button is visible/hidden
+    var restartShowing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,20 +32,47 @@ class WinnersVC: UIViewController {
         // Assign value to labels
         finalGamePoints.text = pointsPassed
         finalGameTime.text = timePassed
-
-        // Sound for game over reached
-        func startGameMusic() {
-            
-            let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "gameover", ofType: "mp3")!)
-            
-            do {
-                gameOver = try AVAudioPlayer(contentsOf: url)
-                gameOver.prepareToPlay()
-                gameOver?.play()
-                gameOver?.numberOfLoops = 1
-            } catch let error as NSError {
-                print("audioPlayer error \(error.localizedDescription)")
-            }
+        
+//        // Sound for game over reached
+//        func startGameMusic() {
+//            
+//            let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "gameover", ofType: "mp3")!)
+//            
+//            do {
+//                gameOver = try AVAudioPlayer(contentsOf: url)
+//                gameOver.prepareToPlay()
+//                gameOver?.play()
+//                gameOver?.numberOfLoops = 1
+//            } catch let error as NSError {
+//                print("audioPlayer error \(error.localizedDescription)")
+//            }
+//        }
+    }
+    
+    // Set gesture tap to show reset button when screen is tapped
+    @IBAction func tapScreenToRestart(_ sender: UIButton) {
+        if restartShowing {
+            UIView.animate(withDuration: 0.7, animations: {
+                self.backgroundButton.alpha = 0.6
+                self.resetButton.alpha = 1
+                self.view.layoutIfNeeded()
+            })
+        } else {
+            UIView.animate(withDuration: 0.7, animations: {
+                self.backgroundButton.alpha = 0.01
+                self.resetButton.alpha = 0
+                self.view.layoutIfNeeded()
+            })
         }
+        
+        // Allows toggle
+        restartShowing = !restartShowing
+    }
+    
+    // Reset game button to main menu
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        pokeMatchVC.resetGame()
+        dismiss(animated: true, completion: nil)
+//        navigationController?.popToRootViewController(animated:true)
     }
 }
