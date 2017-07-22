0786f1e81f33ace6766bb5abb3eb137bc3993ab9
Allen Boynton <alboynton4@gmail.com>
2017-07-22 01:07:34 -0400

    Added methods for Cloud messaging

--- PokeMatch/PokeMatch/AppDelegate.swift
+++ PokeMatch/PokeMatch/AppDelegate.swift
@@ -18,23 +18,88 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
         
-        UNUserNotificationCenter.current().delegate = self
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {granted, error in
+                    
+                if granted {
+                    print("Notification access granted")
+                } else {
+                    print(error?.localizedDescription as Any)
+                }
+            })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
         
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {granted, error in
+        application.registerForRemoteNotifications()
         
-            if granted {
-                print("Notification access granted")
-            } else {
-                print(error?.localizedDescription as Any)
+        // Retrieve the current registration token
+        let token = Messaging.messaging().fcmToken
+        print("FCM token: \(token ?? "")")
+        
+        // Monitor token generation
+        func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+            print("Firebase registration token: \(fcmToken)")
+        }
+        
+        // Provide your APNs token using the APNSToken property:
+        func application(application: UIApplication,
+                         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+            Messaging.messaging().apnsToken = deviceToken as Data
+        }
+        
+        // Asks permission to show notifications
+        func askToShowVisibleNotifications() {
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { ( granted, error) in
+                
             }
-        })
+        }
         
+        // Swizzle - Build upon your own notifications
+        func swizzled_application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            
+        }
+        
+        func swizzled_userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            
+        }
+        
+        func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                         fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+            // If you are receiving a notification message while your app is in the background,
+            // this callback will not be fired till the user taps on the notification launching the application.
+            // TODO: Handle data of notification
+            
+            // With swizzling disabled you must let Messaging know about the message, for Analytics
+            Messaging.messaging().appDidReceiveMessage(userInfo)
+            
+//            Messaging.messaging().sendMessage(message, to: to, withMessageID: messageId, timeToLive: ttl)
+            
+            // Print message ID.
+            if let messageID = userInfo["JDPYMY5287"] {
+                print("Message ID: \(messageID)")
+            }
+            
+            // Print full message.
+            print(userInfo)
+            
+            completionHandler(UIBackgroundFetchResult.newData)
+        }
+                
         return true
     }
     
