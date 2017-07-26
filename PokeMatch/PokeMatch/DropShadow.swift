3c086669457af19ec41d37c68d247ea31cb1e5c0
Allen Boynton <alboynton4@gmail.com>
2017-07-26 17:33:43 -0400

    Added a dropshadow protocol

---
+++ PokeMatch/PokeMatch/DropShadow.swift
@@ -0,0 +1,23 @@
+//
+//  DropShadow.swift
+//  TacoPOP
+//
+//  Created by Allen Boynton on 7/20/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {}
+
+extension DropShadow where Self: UIView {
+    
+    func addDropShadow() {
+        // implementation
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 6
+    }
+}
+
