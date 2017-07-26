acfb7adf876a11a2da9da624351dbc3e75158558
Allen Boynton <alboynton4@gmail.com>
2017-07-26 17:32:02 -0400

    Changed WinnersVC to FinalScoreVC

---
+++ PokeMatch/PokeMatch/BestTimesVC.swift
@@ -0,0 +1,23 @@
+//
+//  BestTimesVC.swift
+//  PokéMatch
+//
+//  Created by Allen Boynton on 7/23/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+class BestTimesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // Go back
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
