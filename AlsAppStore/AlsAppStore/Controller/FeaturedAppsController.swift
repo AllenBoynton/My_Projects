b06024a51a165b89dd757b9e22eea538926401a3
Allen Boynton <alboynton4@gmail.com>
2017-11-08 10:55:01 -0500

    Created cells and collection views
    Created 3 collection views for each row of our display.
    Cells include insets, and CGSize with constraints.

--- AlsAppStore/AlsAppStore/Controller/FeaturedAppsController.swift
+++ AlsAppStore/AlsAppStore/Controller/FeaturedAppsController.swift
@@ -10,20 +10,18 @@ import UIKit
 
 private let cellId = "cellId"
 
-class FeaturedAppsController: UICollectionViewController {
+class FeaturedAppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = UIColor.white
         
-        // register collection cell
         collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
-        
         return cell
     }
     
@@ -31,26 +29,10 @@ class FeaturedAppsController: UICollectionViewController {
         return 3
     }
 
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let width = view.frame.width
-        let height = 150
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = view.frame.width
+        let height: CGFloat = 150.0
         return CGSize(width: width, height: height)
-    }
-}
-
-class CategoryCell: UICollectionViewCell {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = UIColor.red
     }
 }
 
